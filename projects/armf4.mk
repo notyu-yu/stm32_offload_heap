a1ae1d0b0c0cdca5862ae9b8bdcb22baed81cfef
yu <notyuhang@gmail.com>
2022-07-25 16:03:01 -0400

    merged with mpu

--- projects/armf4.mk
+++ projects/armf4.mk
@@ -77,7 +77,7 @@ all: clean $(SRCS) build pc_side size
 build: $(TARGET).elf $(TARGET).bin $(TARGET).lst
 
 pc_side: pc_side/pc_server.c pc_side/pc_request.c pc_side/pc_mm.c pc_side/pc_mlib.c pc_side/dict.c pc_side/dict.h pc_side/memlib.h pc_side/pc_mm.h pc_side/pc_request.h pc_side/uart_comms.h shared_side/shared_config.h
-	gcc -O3 -o pc_server pc_side/pc_server.c pc_side/pc_request.c pc_side/pc_mm.c pc_side/pc_mlib.c pc_side/dict.c
+	gcc -g3 -o pc_server pc_side/pc_server.c pc_side/pc_request.c pc_side/pc_mm.c pc_side/pc_mlib.c pc_side/dict.c
 
 $(TARGET).elf: $(OBJS)
 	$(CC) $(OBJS) $(LDFLAGS) -o $(OBJDIR)/$@
